8370dc7073eb9db4f4702dd486b48ad32cdf9b21
drv98 <drv98@naver.com>
2023-07-10 11:45:10 +0900

    서치박스 입력시 업데이트

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SearchBox from "./components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
 
   //검색어로 영화데이터 요청 async/await는 쌍으로 사용
   const getMovieRequest = async (searchValue) => {
@@ -14,19 +15,24 @@ function App() {
     //자바스크립트는 비동기므로 await를 붙여서 영화데이터를 다받고 다음코드 실행
     const response = await fetch(url);
     const responseJson = await response.json();
-    setMovies(responseJson.Search);
+    if (responseJson.Search) {
+      //결과가 있을경우!
+      setMovies(responseJson.Search);
+    }
   };
 
-  //앱 시작시 한번 함수 실행
+  //검색어가 바뀔시 한번 함수 실행
   useEffect(() => {
-    getMovieRequest("star wars");
-  }, []);
+    if (searchValue.length > 3) {
+      getMovieRequest(searchValue);
+    }
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
       <div className="row align-items-center my-4">
         <MovieListHeading heading="Movies" />
-        <SearchBox />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
         <MovieList movies={movies} />
